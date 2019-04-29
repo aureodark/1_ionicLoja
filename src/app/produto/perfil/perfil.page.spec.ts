8d7862fdcbe20cf824044b8f34e1165f3dd717d8
aureodark <aureofernandes@hotmail.com.br>
2019-04-29 00:18:36 -0300

    29042019

--- src/app/produto/perfil/perfil.page.spec.ts
+++ src/app/produto/perfil/perfil.page.spec.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PerfilPage } from './perfil.page';
+import { PerfilPage } from "./PerfilPage";
 
 describe('PerfilPage', () => {
   let component: PerfilPage;
--- src/app/produto/perfil/perfil.page.ts
+++ src/app/produto/perfil/perfil.page.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import { Produto } from '../produto';
+import { ProdutoDadosService } from '../produto-dados.service';
+
+
 import { ProdutoService } from '../produto.service';
+import { Router } from "@angular/router";
+
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -9,10 +16,68 @@ import { ProdutoService } from '../produto.service';
 })
 export class PerfilPage implements OnInit {
 
-  constructor(private route: ActivatedRoute, private produtoService: ProdutoService) { }
+  produto: Produto;
+  key: string = '';
+
+
+
+
+
+
+
+  @ViewChild('mySlider') mySlider: any;
+
+  elenco = [
+    {
+      "img": "../../assets/img/1.jpg",
+      "posicao": "bala"
+    },
+    {
+      "img": "../../assets/img/2.jpg",
+      "posicao": "goma"
+    },
+    {
+      "img": "../../assets/img/3.jpg",
+      "posicao": "obama"
+    },
+  ]
+
+  slideNext() {
+    this.mySlider.slideNext();
+  }
+
+  slidePrev() {
+    this.mySlider.slidePrev();
+  }
+
+
+
+
+
+
+
+
+
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private produtoDataService: ProdutoDadosService
+  ) { }
 
   ngOnInit() {
-    this.getHero();
+    this.produto = new Produto();
+    this.produtoDataService.currentProduto.subscribe(data => {
+      if (data.produto && data.key) {
+        this.produto = new Produto();
+        this.produto.nome = data.produto.nome;
+        this.produto.descricao = data.produto.descricao;
+        this.produto.quant = data.produto.quant;
+        this.produto.valor = data.produto.valor;
+        this.produto.fotos = data.produto.fotos;
+        this.key = data.key;
+      }
+    })
   }
 
 
